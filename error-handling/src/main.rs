e57d34891fec92eeb8fee96d808e3e5d278dd68f
Yash <bajajy82@gmail.com>
2024-12-07 13:09:49 +0530

    ? operator

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,33 +1,22 @@
 use core::panic;
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, ErrorKind, Read};
+
+fn read_username_from_file()->Result<String, io::Error>{
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+
+    
+
+
+}
+
 
 fn main() {
-    let f = File::open("hello.txt");
+    let f = File::open("hello.txt").expect("Failed to open hell.txt");
 
-    let f = match f {
-        Ok(file)=>file,
-        Err(error)=>match error.kind(){
-            ErrorKind::NotFound => match File::create("hello.txt"){
-                Ok(fc)=>fc,
-                Err(e)=>panic!("Problem creating the file: {:?}", e),
-            },
-
-            other_error =>{
-                panic!("Problem opening the file: {:?}", other_error)
-            }
-        }
-    };
-
-
-    let f = File::open("hello.txt").unwrap_or_else(|error|{
-        if error.kind() == ErrorKind::NotFound{
-            File::create("hello.txt").unwrap_or_else(|error|{
-                panic!("Problem creating the file: {:?}",error);
-            })
-        }else{
-            panic!("Problem opening the file {:?}",error);
-        }
-    });
+    
 
 }

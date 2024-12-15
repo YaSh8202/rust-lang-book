0ac98cbd70a4b32b412ce753e764d78d97b95d3c
Yash <bajajy82@gmail.com>
2024-12-15 20:31:28 +0530

    deref pointer

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 enum List {
     Cons(i32, Box<List>),
     Nil,
@@ -18,8 +20,32 @@ fn print_list(list: List) {
 }
 
 
-fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+struct MyBox<T>(T);
 
-    print_list(list);
+impl<T> MyBox<T> {
+    fn new(x:T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T>{
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn main() {
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    // print_list(list);
+
+    let x = 5;
+
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
 }

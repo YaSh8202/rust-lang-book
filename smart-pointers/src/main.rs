ba28f25ffd051eddef3993d630d898609a752783
Yash <bajajy82@gmail.com>
2024-12-15 21:41:25 +0530

    drop reference

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::Deref;
+use std::{fmt::Display, ops::{Deref, Drop}};
 
 enum List {
     Cons(i32, Box<List>),
@@ -12,27 +12,43 @@ fn print_list(list: List) {
         Cons(i, l) => {
             println!("{}", i);
             print_list(*l);
-        },
+        }
         Nil => {
             println!("Nil");
         }
     }
 }
 
+struct MyBox<T: Display>(T);
 
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x:T) -> MyBox<T> {
+impl<T> MyBox<T> where T: Display {
+    fn new(x: T) -> MyBox<T> {
         MyBox(x)
     }
 }
 
-impl<T> Deref for MyBox<T>{
+impl<T> Deref for MyBox<T> where T: Display {
     type Target = T;
 
     fn deref(&self) -> &T {
         &self.0
+    }
+}
+
+impl<T> Drop for MyBox<T> where T:Display {
+   fn drop(&mut self) {
+         println!("Dropping MyBox with data: {}", self.0);
+   }
+}
+
+
+struct MyStruct{
+    data: i32
+}
+
+impl Display for MyStruct {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "MyStruct with data: {}", self.data)
     }
 }
 
@@ -45,7 +61,17 @@ fn main() {
 
     let y = MyBox::new(x);
 
+    let m = MyStruct{data: 10};
+
+    let m_box = MyBox::new(m);
+
     assert_eq!(5, x);
     assert_eq!(5, *y);
+    // assert_eq!(m, *m_box);
+
+    drop(m_box);
+
+    println!("End of main");
+
 
 }

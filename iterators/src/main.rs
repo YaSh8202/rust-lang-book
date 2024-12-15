89d505f504906918093054dfae5369fc393b1183
Yash <bajajy82@gmail.com>
2024-12-15 15:26:57 +0530

    iterators and closures

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,8 +1,30 @@
+#[derive(PartialEq, Debug)]
+struct Shoe{
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+
 fn main() {
     
-    let v1 = vec![1, 2, 3];
+    let shoes = vec![
+        Shoe{size: 10, style: String::from("sneaker")},
+        Shoe{size: 13, style: String::from("sandal")},
+        Shoe{size: 10, style: String::from("boot")},
+    ];
 
-    let v2 : Vec<_>= v1.iter().map(|x| x +1).collect();
+    let in_my_size = shoes_in_my_size(shoes, 10);
 
-    assert_eq!(v2, vec![2, 3, 4]);
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe{size: 10, style: String::from("sneaker")},
+            Shoe{size: 10, style: String::from("boot")},
+        ]
+    );
+    println!("{:?}", in_my_size);
 }

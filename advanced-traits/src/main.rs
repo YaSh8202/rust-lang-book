b3cb2b3e7f99eba4fae08ab998495123761feef3
Yash <bajajy82@gmail.com>
2024-12-25 22:14:24 +0530

    calling methods with the same name

--- advanced-traits/src/main.rs
+++ advanced-traits/src/main.rs
@@ -35,17 +35,63 @@ impl Display for Millimeters{
     }
 }
 
+
+trait Pilot{
+    fn fly(&self);
+}
+
+trait Wizard{
+    fn fly(&self);
+
+    fn assosiate();
+}
+
+struct Human;
+
+impl Human{
+    fn fly(&self){
+        println!("*waving arms furiously*");
+    }
+}
+
+impl Pilot for Human{
+    fn fly(&self){
+        println!("This is your captain speaking");
+    }
+}
+
+impl Wizard for Human{
+    fn fly(&self){
+        println!("Up!");
+    }
+
+    fn assosiate(){
+        println!("fly");
+    }
+}
+
 fn main(){
-    let p1 = Point { x: 1, y: 0 };
-    let p2 = Point { x: 2, y: 3 };
-    let p3 = p1 + p2;
-    assert_eq!(p3, Point { x: 3, y: 3 });
-    println!("{:?}", p3);
+    // let p1 = Point { x: 1, y: 0 };
+    // let p2 = Point { x: 2, y: 3 };
+    // let p3 = p1 + p2;
+    // assert_eq!(p3, Point { x: 3, y: 3 });
+    // println!("{:?}", p3);
 
 
-    let mm = Millimeters(1000);
-    let m = Meters(1);
+    // let mm = Millimeters(1000);
+    // let m = Meters(1);
 
-    let mm_plus_m = mm + m;
-    println!("{}", mm_plus_m);
+    // let mm_plus_m = mm + m;
+    // println!("{}", mm_plus_m);
+
+
+    let person = Human;
+
+    person.fly();
+
+    Pilot::fly(&person);
+
+    Wizard::fly(&person);
+
+    <Human as Wizard>::assosiate(); // syntax for calling associated function
 }
